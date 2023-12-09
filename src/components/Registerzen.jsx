ac14fdd8af60caa5ed2f7d566198e03b2dd10185
Aswin-Annalath1 <aswinannalath2001@gmail.com>
2023-12-09 20:06:08 +0530

    changes

--- src/components/Registerzen.jsx
+++ src/components/Registerzen.jsx
@@ -75,7 +75,7 @@ if (/^\d{6}$/.test(password)) {
                 placeholder="******************"
                 required
               />
-              <p className="text-red-500 text-xs italic">Please choose a password.</p>
+              <p className="text-red-500 text-xs italic">Please choose a 6-digit password.</p>
             </div>
             <div className="flex items-center justify-between">
             <button type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" 
